daf8a163b5b0ec9565a2aa77ace20d33e34aa31d
limaarthur <artglima.dev@gmail.com>
2025-04-29 12:14:15 -0300

    feat: connecting to axios client api and variable configuration with vite

--- src/pages/Catalog/ProductDetails/index.tsx
+++ src/pages/Catalog/ProductDetails/index.tsx
@@ -1,43 +1,50 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../../../core/utils/requests';
 import { Link } from 'react-router';
-
 import ArrowIcon from '../../../core/assets/images/arrow-icon.svg?react';
-import ProductImage from '../../../core/assets/images/product.svg';
-
 import { Header } from '../../../core/components/Header';
-
-import styles from './ProductDetails.module.css';
 import { ProductPrice } from '../../../core/components/ProductPrice';
+import type { Product } from '../../../core/types/products';
+import styles from './ProductDetails.module.css';
 
 export function ProductDetails() {
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/products/2`)
+    .then(response => {
+      setProduct(response.data);
+    });
+  }, []);
+
   return (
     <>
       <Header />
       <div className={styles.productDetailsContainer}>
         <div className={styles.cardDetailsProduct}>
-          <Link to="/catalog" className={styles.productDetailsGoback}>
+          <Link to="/products" className={styles.productDetailsGoback}>
             <ArrowIcon className={styles.iconGoback} />
             <h1 className={styles.textGoback}>voltar</h1>
           </Link>
           <div className={styles.productDetailsInfo}>
             <div className={styles.productInfo}>
               <div className={styles.productDetailsCard}>
-                <img src={ProductImage} alt="Desktop" className={styles.productDetailsImage} />
+                <img src={product?.imgUrl} alt={product?.name} className={styles.productDetailsImage} />
               </div>
               <div className={styles.productInfoFields}>
                 <h1 className={styles.productDetailsName}>
-                  Computador Desktop - Intel Core i7
+                  {product?.name}
                 </h1>
-                <ProductPrice />
+                {product && <ProductPrice price={product?.price} />}
               </div>
             </div>
             <div className={styles.productDescriptionCard}>
               <h1 className={styles.productDescriptionTitle}>
-                Título da descrição do produto
+                Descrição do produto
               </h1>
               <p className={styles.producuDescriptionText}>
-                Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. 
-                A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. 
-                Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.
+                {product?.description}
               </p>
             </div>
           </div>
