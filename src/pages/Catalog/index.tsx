90b244408faf49f8675e728d84f2f92cc89af5bd
limaarthur <artglima.dev@gmail.com>
2025-04-30 20:30:51 -0300

    feat: requesting card data via api

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -9,6 +9,7 @@ import type { Product } from '../../core/types/products';
 import styles from './Catalog.module.css';
 import type { SpringPage } from '../../core/types/spring';
 import type { AxiosParams } from '../../core/types/vendor/axios';
+import { Link } from 'react-router';
 
 export function Catalog() {
   const [page, setPage] = useState<SpringPage<Product>>();
@@ -29,27 +30,6 @@ export function Catalog() {
     })
   }, []);
 
-  const product: Product = {
-    id: 2,
-     name: 'Smart TV',
-     description:
-       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-     price: 2190.0,
-     imgUrl:
-       'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-     date: '2020-07-14T10:00:00Z',
-     categories: [
-       {
-         id: 1,
-         name: 'Livros',
-       },
-       {
-         id: 3,
-         name: 'Computadores',
-       },
-     ],
-  };
-  
   return (
     <>
       <Header />
@@ -60,18 +40,19 @@ export function Catalog() {
           </h1>
           <ProductFilters />
         </div>
+
         <div className={styles.catalogProducts}>
-          <ProductCard product={product} />
-          <ProductCard product={product} />
-          <ProductCard product={product} />
-          <ProductCard product={product} />
-          <ProductCard product={product} />
-          <ProductCard product={product} />
-          <ProductCard product={product} />
-          <ProductCard product={product} />
-          <ProductCard product={product} />
-          <ProductCard product={product} />
+          {page?.content.map(product => {
+            return (
+              <div key={product.id}>
+                <Link to={'/products/1'}>
+                  <ProductCard product={product} />
+                </Link>
+              </div>
+            )
+          })}
         </div>
+      
         <Pagination />
       </div>
     </>
