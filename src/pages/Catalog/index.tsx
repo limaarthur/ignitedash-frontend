0e452bde8330d7b28f08071faf156689007ce54c
limaarthur <artglima.dev@gmail.com>
2025-04-30 13:50:57 -0300

    feat: creation of paginated request

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { ProductFilters } from '../../core/components/ProductFilters';
 import { ProductCard } from './components/ProductCard';
 import { Header } from '../../core/components/Header'
 import { Pagination } from '../../core/components/Pagination';
-
-import styles from './Catalog.module.css';
+import { BASE_URL } from '../../core/utils/requests';
 import type { Product } from '../../core/types/products';
+import styles from './Catalog.module.css';
+import type { SpringPage } from '../../core/types/spring';
+import type { AxiosParams } from '../../core/types/vendor/axios';
 
 export function Catalog() {
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const params: AxiosParams = {
+      method: 'GET',
+      url: `${BASE_URL}/products`,
+      params: {
+        page: 0,
+        size: 12,
+      },
+    };
+
+    axios(params).then((response) => {
+      setPage(response.data);
+      console.log(response.data)
+    })
+  }, []);
+
   const product: Product = {
     id: 2,
      name: 'Smart TV',
