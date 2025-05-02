deda52df7e9663a1dfab17e229e399791e95008d
limaarthur <artglima.dev@gmail.com>
2025-05-02 12:52:21 -0300

    feat: creating the authentication page

---
+++ src/pages/Admin/Auth/index.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'react-router';
+import { Login } from './components/Login';
+import AuthImage from '../../../core/assets/images/auth-image.svg?react';
+
+import styles from './Auth.module.css';
+
+export function Auth() {
+  return (
+    <div className={styles.authContainer}>
+      <div className={styles.authBannerContainer}>
+        <h1 className={styles.authBannerContainerTitle}>
+          Divulgue seus produtos <br /> no Ignitedash
+        </h1>
+        <p className={styles.authBannerContainerSubTitle}>
+          Faça parte do nosso catálogo de divulgação e <br /> aumente a venda dos seus produtos.
+        </p>
+        <AuthImage />
+      </div>
+      <div className={styles.authFormContainer}>
+        <Routes>
+          <Route path="/admin/auth/login" element={<Login />} />
+          <Route path="/admin/auth/signup" element={<h1>Card de signup</h1>} />
+          <Route path="/admin/auth/recover" element={<h1>Card de recover</h1>} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router";
-
 import { Home } from "./pages/Home";
 import { Admin } from "./pages/Admin";
 import { Catalog } from "./pages/Catalog";
 import { ProductDetails } from "./pages/Catalog/ProductDetails";
+import { Auth } from "./pages/Admin/Auth";
 
 export function AppRoutes() {
   return (
@@ -13,6 +13,7 @@ export function AppRoutes() {
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/products/:productId" element={<ProductDetails />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/auth/*" element={<Auth />} />
       </Routes>
     </BrowserRouter>
   )
