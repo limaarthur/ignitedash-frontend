21831ded9bd1edf28f97c99d4c16dda2b0bbce44
limaarthur <artglima.dev@gmail.com>
2025-05-02 19:01:44 -0300

    feat: receiving form data with react hook form

--- src/pages/Admin/Auth/components/Login/index.tsx
+++ src/pages/Admin/Auth/components/Login/index.tsx
@@ -1,27 +1,45 @@
+import { useForm } from 'react-hook-form';
 import { Link } from "react-router";
 import { ButtonLogin } from "../../../../../core/components/ButtonLogin";
 import { AuthCard } from "../AuthCard";
 
 import styles from './Login.module.css';
 
+type FormData = {
+  username: string
+  password: string
+}
+
 export function Login() {
+  const { register, handleSubmit } = useForm<FormData>();
+  const onSubmit = (formData : FormData) => {
+    console.log(formData);
+  };
+  
   return (
     <div className={styles.loginContainer}>
       <AuthCard title="login">
-        <form className={styles.loginForm}>
+
+        <form 
+          onSubmit={handleSubmit(onSubmit)} 
+          className={styles.loginForm}
+        >
+
           <div className={styles.loginFormEmailContent}>
             <input 
+              {...register("username")}
               type="email"
               placeholder="Email"
-              name="username"
+              //name="username"
             />
           </div>
 
           <div className={styles.loginFormPasswordContent}>
             <input 
+              {...register("password")}
               type="password"
               placeholder="Senha"
-              name="password" 
+              //name="password" 
             />
           </div>
 
